bea981ea2c876731f180ae857f7d99b97ced39bc
Ezra Luvaha <mrluvaha@gmail.com>
2023-11-19 17:12:50 +0300

    4th commit

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,7 @@ const dotenv = require("dotenv").config();
 const Stripe = require('stripe')
 
 const app = express();
-// Enable CORS for all routes
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://freshharvest-topaz.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
-
+app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 
 const PORT = process.env.PORT || 8080;
