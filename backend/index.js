173cdfb10d083ecd4fcc308879bb4782965ee2f0
Ezra Luvaha <mrluvaha@gmail.com>
2023-11-19 16:47:28 +0300

    CORS update index.js(backend)

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,13 @@ const dotenv = require("dotenv").config();
 const Stripe = require('stripe')
 
 const app = express();
-app.use(cors(
-  {
-    origin : ["https://freshharvest-topaz.vercel.app/"],
-    methods : ["POST", "GET"],
-    credentials : true
-  }
-));
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://freshharvest-topaz.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.use(express.json({ limit: "10mb" }));
 
